623cdc44416cfb6048d7f5f490851f174827f6b0
yasir5405 <yasirnaseem5415@gmail.com>
2024-12-04 18:34:12 +0530

    Basics _01Program

---
+++ _01Program.java
@@ -0,0 +1,10 @@
+// Traversing an array
+class _01Program{
+    public static void main(String[] args){
+        int [] arr = {10,20,30,40,50};
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println("Element at index " + i + " is " + arr[i]);
+        }
+    }
+}
